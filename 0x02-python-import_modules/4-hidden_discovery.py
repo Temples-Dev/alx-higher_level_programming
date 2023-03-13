acc56cb99c3ca25abf7e4669248c9d892028a093
Temples-Dev <templelomotey@gmail.com>
2023-03-12 18:00:01 -0700

    mm

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if not i.startswith("__"):
-            print("{:s}".format(i))
+    import hidden_4 as hidden
+    list = dir(hidden)
+    for x in range(len(list)):
+        if(list[x][0] != '_'):
+            print(list[x])
