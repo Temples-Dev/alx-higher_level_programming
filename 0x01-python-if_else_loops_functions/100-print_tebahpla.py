4a37980db94cd4b3750d7b1c957a45d5772b0c84
Temples-Dev <templelomotey@gmail.com>
2023-03-12 17:27:44 -0700

    modified2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for x in range(ord("z"), ord("a") - 1, -1):
+    print("{}".format(chr(x - (32 if x % 2 == 1 else 0))), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(100):
-    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
-        print("{}{}".format(int(i / 10), i % 10), end="")
-        if (i != 89):
-            print(", ", end="")
-print("")
+for i in range(10):
+    for j in range(10):
+        if j >= i and i != j:
+            if i == 8 and j == 9:
+                print(89)
+            else:
+                print("{}".format(str(i) + str(j)), end=", ")
