2868e9b37281695f8a943e3fc37fa341da954298
Temples-Dev <templelomotey@gmail.com>
2023-03-21 05:42:12 -0700

    😢

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
 
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
 
-if __name__ == '__main__':
-    import sys
-    from calculator_1 import add, sub, mul, div
+    op = argv[2]
+    f = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in f:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-    if len(sys.argv) != 4:
-        print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
-        sys.exit(1)
-    if sys.argv[2] not in ['+', '-', '*', '/']:
-        print('Unknown operator. Available operators: +, -, * and /')
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == '+':
-        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    else:
-        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
