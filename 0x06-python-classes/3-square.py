2de5ebc1f427507ca4cb22055b828a314942cda4
Temples-Dev <templelomotey@gmail.com>
2023-03-29 15:32:16 -0700

    modf

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,13 +5,30 @@
 class Square:
     """Area of a square"""
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
 
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
 
     def area(self):
+
         return (self.__size ** 2)
+
+
+
+
+
+    # def __init__(self, size=0):
+    #     if type(size) != int:
+    #         raise TypeError("size must be an integer")
+    #     elif size < 0:
+    #         raise ValueError("size must be >= 0")
+    #     else:
+    #         self.__size = size
+
+
+    # def area(self):
+    #     return (self.__size ** 2)
